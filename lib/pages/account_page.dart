e910f94e8110fe499df9c46bd98db5f5aa2525ea
davewg1 <78000662+davewg1@users.noreply.github.com>
2023-06-07 15:02:27 +0800

    Account TextFields and packages for OCR

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class UserPage extends StatelessWidget {
   const UserPage({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
+
+    final name = TextFormField(
+      autofocus: false,
+      //controller: nameController,
+      keyboardType: TextInputType.name,
+      onSaved: (value){
+        //nameController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Name",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        )
+      ),
+    );
+    final address = TextFormField(
+      autofocus: false,
+      //controller: addressController,
+      onSaved: (value){
+        //addressController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Address",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+          )
+      ),
+    );
+    final birthdate = TextFormField(
+      autofocus: false,
+      //controller: birthdateController,
+      onSaved: (value){
+        //birthdateController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Birth Date",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+          )
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Row(
@@ -25,6 +75,21 @@ class UserPage extends StatelessWidget {
           bottom: BorderSide(color: Colors.grey, width: 2),
         ),
       ),
+      body: Center(
+        child:SingleChildScrollView(
+            child:Container(
+            color: Colors.white,
+            child: Padding(padding:const EdgeInsets.all(36.0),
+                child: Form(
+                    child: Column(
+                      children: <Widget>[Text("Name"),name,Text("Address"),address,Text("Birth"),birthdate],
+                    )
+                )
+            )
+          )
+        )
+      )
+
     );
   }
 }
